c7cabbfb59769025386afc495645e8a1ea6fa4ee
Arya Irani <arya.irani@gmail.com>
2014-10-10 19:24:36 -0400

    whitespace

--- src/main/scala/net/arya/intern/Intern.scala
+++ src/main/scala/net/arya/intern/Intern.scala
@@ -9,6 +9,7 @@ import java.util.concurrent.ConcurrentHashMap
  */
 object Intern {
   private val map = new ConcurrentHashMap[Manifest[_],Intern[Any]]()
+
   def apply[A](a: A)(implicit m: Manifest[A]): A = {
     map.putIfAbsent(m, new Intern[Any])
     map.get(m).asInstanceOf[Intern[A]](a)
@@ -19,9 +20,12 @@ object Intern {
   /** returns the population of the intern cache, by type */
   def info = map.mapValues(_.size)
 }
+
+
 class Intern[A] {
   private val map = new WeakHashMap[A,WeakReference[A]]
   def size = map.size
+
   def apply(a: A): A = {
     if (map.containsKey(a)) map.get(a).get()
     else { map.put(a, new WeakReference[A](a)); a }
@@ -31,12 +35,15 @@ class Intern[A] {
 trait InternDeep[A] {
   def internDeep(a: A): A
 }
+
 object InternDeep {
+
   implicit def list[A:Manifest] = new InternDeep[List[A]] {
     def internDeep(l: List[A]): List[A] =
       l.foldRight(List.empty[A]) {
         case (e, l) => Intern(e :: l)
       }
   }
+
 }
 
